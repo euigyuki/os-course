d6b585ac281b74a82bfb00ca4786e3616167fc20
derrick kim <derrickkim7607@gmail.com>
2025-04-25 23:05:27 -0400

    deleted edit pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,6 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         // 🚫 Blog removed completely
         theme: {
